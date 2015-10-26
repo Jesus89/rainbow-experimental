ba5aee7924e4132fe725d686831e61f53359a7f8
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2015-10-26 15:19:49 +0100

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rainbowClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rainbowClientApp
+ */
+angular.module('rainbowClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
