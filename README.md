9e0b02428e4d5ff38ea36623605b743e9408446f
Jesús Arroyo Torrens <jesus.arroyo@bq.com>
2015-10-27 09:56:20 +0100

    Bottle example added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 Rainbow server
+
+## Installation
+
+### Python requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+## Execute
+
+Launch the server
+
+```bash
+python launcher.py
+```
---
+++ launcher.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+try:
+    import bottle
+except ImportError as e:
+    print e.message
+    exit(1)
+
+from rainbow_server.app import server
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
