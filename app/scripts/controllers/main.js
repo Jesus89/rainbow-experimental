7120704c17f606417a4ad86992a91df3cca24394
Tom Calvo <calvo.tom@gmail.com>
2015-10-27 14:08:14 +0100

    basic rest client

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,22 @@
  * Controller of the rainbowClientApp
  */
 angular.module('rainbowClientApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function($scope, $http) {
+        $scope.requestParams = {
+            method: 'GET',
+            url: 'http://localhost:8080'
+        }
+
+        $scope.successResponse = '';
+        $scope.errorResponse = '';
+
+        $scope.doRequest = function() {
+            $http($scope.requestParams).then(function(data) {
+                $scope.successResponse = JSON.stringify(data);
+                $scope.errorResponse = '';
+            }, function(data) {
+                $scope.successResponse = '';
+                $scope.errorResponse = JSON.stringify(data);
+            });
+        }
+    });
