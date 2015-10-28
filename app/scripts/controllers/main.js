05ee008b7fa424b75d403670626db3bf937ddea0
Tom Calvo <calvo.tom@gmail.com>
2015-10-28 09:06:42 +0100

    Dynamic methods form generation by first request

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,19 +11,52 @@ angular.module('rainbowClientApp')
     .controller('MainCtrl', function($scope, $http) {
         $scope.requestParams = {
             method: 'GET',
-            url: 'http://localhost:8080'
+            url: 'http://localhost:8081'
         }
 
-        $scope.successResponse = '';
-        $scope.errorResponse = '';
+        $scope.successResponse = null;
+        $scope.errorResponse = null;
+        $scope.successResponseText = '';
+        $scope.errorResponseText = '';
+
+        $scope.parameters = [];
 
         $scope.doRequest = function() {
             $http($scope.requestParams).then(function(data) {
-                $scope.successResponse = JSON.stringify(data);
-                $scope.errorResponse = '';
+                $scope.successResponse = data;
+                $scope.successResponseText = JSON.stringify(data);
+                $scope.errorResponse = null;
+                $scope.errorResponseText = '';
+                console.log($scope.successResponse);
             }, function(data) {
-                $scope.successResponse = '';
-                $scope.errorResponse = JSON.stringify(data);
+                $scope.successResponse = null;
+                $scope.successResponseText = '';
+                $scope.errorResponseText = JSON.stringify(data);
+            });
+        }
+
+        $scope.stringify = function(object) {
+            return JSON.stringify(object);
+        }
+
+        $scope.doMethodRequest = function(key) {
+            var requestParams = {
+                method: $scope.requestParams.method,
+                url: $scope.requestParams.url + '/' + key
+            };
+            if (requestParams.method === 'GET') {
+                requestParams.params = $scope.parameters[key];
+            } else {
+                requestParams.data = $scope.parameters[key];
+            }
+
+            $http(requestParams).then(function(data) {
+                console.log('allOK');
+                $scope.methodSuccessResponseText = JSON.stringify(data);
+            }, function(data) {
+                console.log('wrong');
+                $scope.methodErrorResponseText = JSON.stringify(data);
+                console.log(data);
             });
         }
     });
